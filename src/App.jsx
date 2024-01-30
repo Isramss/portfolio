ed66ae9fd8c25c4f7cc9213ea3dd9dad1a499554
Isramss <isra.msadak@gmail.com>
2024-01-30 16:44:46 +0100

    modification des images + import // ajout du composant Projet => tableaux des projets + map

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,9 @@
 import Header from "./Components/Header";
 import Skill from "./Components/Skils";
-import Projet from "./Components/Projets";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
-
+import Projets from "./Components/Projets";
 import "./App.css";
-
-const projets = [
-  {
-    name: "AIRBNB-CLONE-2023",
-    imageUrl: "src/assets/portfolio-dev/airbnb-clone-2023-360e5996.jpg.jpg",
-    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
-  },
-  {
-    name: "PORTFOLIO",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → portfolio-website-5e5ff6dc.jpg.jpg",
-    langues: "TYPESCRIPT  REACT",
-  },
-  {
-    name: "POMODORO APP",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → Pomodoro-app-bdd41802.jpg.jpg",
-    langues: "TAILWIND  TYPESCRIPT  REACT",
-  },
-  {
-    name: "EDITEUR MARKDOWN",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → In-browser-markdown-editor-5dffac4c.jpg.jpg",
-    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
-  },
-  {
-    name: "REST COUNTRIES API",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → REST-Countries-API-3c61139c.jpg.jpg",
-    langues: "CSS  TYPESCRIPT  VUE",
-  },
-  {
-    name: "APPLICATION DE CALCULATRICE",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → Calculator-App-671f73d2.jpg.jpg",
-    langues: "SCSS  TYPESCRIPT  VUE",
-  },
-  {
-    name: "DRUM MACHINE",
-    imageUrl: "src/assets/portfolio-dev/Drum-machine-39e3fb93.jpg.jpg",
-    langues: "STYLED  COMPONOENTS  TYPESCRIPT  REACT",
-  },
-  {
-    name: "SITE DE TOURISME DANS L'ESPACE",
-    imageUrl: "src/assets/portfolio-dev/Space-tourism-website-2d4e3d9a.jpg.jpg",
-    langues: "HTML  CSS  TYPESCRIPT",
-  },
-
-  {
-    name: "RANDOM QUOTE APP",
-    imageUrl:
-      "src/assets/portfolio-dev/Section → Random-quote-app-6953f22c.jpg.jpg",
-    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
-  },
-];
-
 function App() {
   return (
     <>
@@ -75,20 +18,7 @@ function App() {
         <h1 className="titre_p">Projet</h1>
         <button className="btn_contact">CONTACTEZ MOI</button>
       </div>
-
-      <div className="grid">
-        {projets.map((projet, index) => {
-          return (
-            <Projet
-              key={index}
-              name={projet.name}
-              imageUrl={projet.imageUrl}
-              langues={projet.langues}
-            />
-          );
-        })}
-      </div>
-
+      <Projets />
       <Contact />
       <Footer />
     </>
--- src/Components/Projets.jsx
+++ src/Components/Projets.jsx
@@ -1,17 +1,86 @@
-function Projet({ name, imageUrl, langues }) {
+import React from "react";
+import aibnbclone from "../assets/portfolio-dev/airbnb-clone.jpg";
+import portfoliowebsite from "../assets/portfolio-dev/portfolio-website.jpg";
+import Pomodoro from "../assets/portfolio-dev/Pomodoro-app.jpg";
+import browser from "../assets/portfolio-dev/In-browser-markdown.jpg";
+import API from "../assets/portfolio-dev/REST-Countries-API.jpg";
+import Calculator from "../assets/portfolio-dev/Calculator-App.jpg";
+import DrumMachine from "../assets/portfolio-dev/Drum-machine.jpg";
+import SpaceTourisme from "../assets/portfolio-dev/Space-tourism-website.jpg";
+import Random from "../assets/portfolio-dev/Random-quote-app.jpg";
+import Projet from "./Projet";
+
+const projets = [
+  {
+    name: "AIRBNB-CLONE-2023",
+    imageUrl: aibnbclone,
+    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
+  },
+  {
+    name: "PORTFOLIO",
+    imageUrl: portfoliowebsite,
+    langues: "TYPESCRIPT  REACT",
+  },
+  {
+    name: "POMODORO APP",
+    imageUrl: Pomodoro,
+    langues: "TAILWIND  TYPESCRIPT  REACT",
+  },
+  {
+    name: "EDITEUR MARKDOWN",
+    imageUrl: browser,
+    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
+  },
+  {
+    name: "REST COUNTRIES API",
+    imageUrl: API,
+    langues: "CSS  TYPESCRIPT  VUE",
+  },
+  {
+    name: "APPLICATION DE CALCULATRICE",
+    imageUrl: Calculator,
+    langues: "SCSS  TYPESCRIPT  VUE",
+  },
+  {
+    name: "DRUM MACHINE",
+    imageUrl: DrumMachine,
+    langues: "STYLED  COMPONOENTS  TYPESCRIPT  REACT",
+  },
+  {
+    name: "SITE DE TOURISME DANS L'ESPACE",
+    imageUrl: SpaceTourisme,
+    langues: "HTML  CSS  TYPESCRIPT",
+  },
+
+  {
+    name: "RANDOM QUOTE APP",
+    imageUrl: Random,
+    langues: "TAILWIND  TYPESCRIPT  REACT  MONGODB",
+  },
+];
+
+function Projets() {
   return (
-    <>
-      <div>
-        <div className="images">
-          <img className="image_grid" src={imageUrl} alt="" />
-        </div>
-        <div className="text_projets">
-          <h3 className="titre_projet">{name}</h3>
-          <div className="text_projet">{langues}</div>
-        </div>
+    <div>
+      <div className="title_andBtn">
+        <h1 className="titre_p">Projet</h1>
+        <button className="btn_contact">CONTACTEZ MOI</button>
       </div>
-    </>
+
+      <div className="grid">
+        {projets.map((projet, index) => {
+          return (
+            <Projet
+              key={index}
+              name={projet.name}
+              imageUrl={projet.imageUrl}
+              langues={projet.langues}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
-export default Projet;
+export default Projets;
